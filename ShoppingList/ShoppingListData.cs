0db40c7afed542172a780e31e2ef43463e04aa13
Amanda Dreesen <akdreesen@users.campus>
2020-02-24 17:12:57 -0600

    In-class work

---
+++ ShoppingList/ShoppingListData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ShoppingList
+{
+    class ShoppingListData : INotifyPropertyChanged
+    { 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        List<string> items = new List<string>();  //reference type
+
+        // public IEnumerable<string> Items => items;   //does this provide data hiding?  No 
+        public IEnumerable<string> Items => items.ToArray();
+
+        public void Add(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+
+        public void Remove(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+
+    }
+}
